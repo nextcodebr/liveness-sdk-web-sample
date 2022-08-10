5a118aed111fa90f1c36cdf85b84ec42ff554198
Thiago de Souza Peixoto <thiago@MacBookPro-2.local>
2022-08-10 17:51:01 -0300

    v5.1

--- src/Livenessv5.1.js
+++ src/Livenessv5.1.js
@@ -344,7 +344,7 @@ class Liveness {
 
     this.ellipseMaskWidth = eyesInner.height * 1.45
     this.ellipseMaskHeight = eyesInner.width / 1.36
-    this.ellipseMaskTop = ((frameBox.top + frameBox.height) / 2.1)
+    this.ellipseMaskTop = eyesOutter.top + (eyesOutter.height / 2)
     this.ellipseMaskLeft =  (frameBox.left + (this.ellipseMaskWidth * 1.24))
     this.ellipseMaskLineWidth = 2
 
